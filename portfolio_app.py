faac6a5da84789ea222b1b05750492df8cbb2346
HamzaAbuSaleh2004 <22110077@htu.edu.jo>
2026-01-26 11:16:44 +0300

    portfolio_app.py

--- portfolio_app.py
+++ portfolio_app.py
@@ -403,10 +403,9 @@ def api_cv_demo_chat():
         return jsonify({"error": str(e)}), 500
 
 
-if __name__ == '__main__':
-    # Print configuration
+if __name__ == "__main__":
     config.print_config()
-    
-    print("Running on: http://127.0.0.1:5000\n")
-    
-    app.run(debug=config.DEBUG, port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=config.DEBUG)
+
+
